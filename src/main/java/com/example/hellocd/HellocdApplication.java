b8351bbd297d4c2c95afb68ed2a1248e00ad69eb
mario_hevia <mario.hevia@gmail.com>
2024-09-16 21:45:11 +0000

    added new mensaje

--- src/main/java/com/example/hellocd/HellocdApplication.java
+++ src/main/java/com/example/hellocd/HellocdApplication.java
@@ -14,7 +14,7 @@ public class HellocdApplication {
 
 	@RequestMapping("/")
 	String home() {
-		return "Hello World!";
+		return "Hello Continuous Delivery!!! ARGH!!";
 	}
 
 	public static void main(String[] args) {
